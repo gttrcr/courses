8372c0fbd8615c131a7a6a0efeff63389c82eb2e
gttrcr <gttrcr@gmail.com>
2026-04-01 17:25:08 +0200

    ms

--- cybersecurity/memory_safety.c
+++ cybersecurity/memory_safety.c
@@ -2,9 +2,14 @@
 #include <string.h>
 int main()
 {
-	char buffer[10];
+	char buffer[1];
 	printf("Inserisci qualcosa: ");
 	gets(buffer);
-	printf("Hai scritto: %s\n", buffer);
+	for(int i=0; i<100; i++)
+		printf("%c", *(buffer+i));
+
+	printf("%d\n", &buffer);
+	//fgets(buffer, sizeof(buffer), stdin);
+	//printf("Hai scritto: %s\n", buffer);
 	return 0;
 }
